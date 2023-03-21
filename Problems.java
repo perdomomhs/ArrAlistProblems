c1e421dc74591984e5b998ca7fd9d89713643d0b
Paul Perdomo <pperdomo@magnoliaisd.org>
2023-03-21 18:33:02 -0500

    First Problems

---
+++ Problems.java
@@ -0,0 +1,31 @@
+public class Problems {
+
+    /* 1-Point Penalty
+        v) Array/collection access confusion ([] get)
+        w) Extraneous code that causes side-effect (e.g., printing to output, incorrect precondition check)
+        x) Local variables used but none declared
+        y) Destruction of persistent data (e.g., changing value referenced by parameter)
+        z) Void method or constructor that returns a value
+     */
+
+
+    /******
+     * Array Problems 
+     *****/
+
+     public static double mean(int[] arr){
+
+     }
+
+     //Hint: Sort it first
+     public static int median(int[] arr){
+
+     }
+
+     //Hint: Sort it first
+     public static int mode(int[] arr){
+
+     }
+
+     
+}
